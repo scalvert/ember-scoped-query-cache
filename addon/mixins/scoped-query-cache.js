07cd3ce73bf1c4e337526088c045dd6de6c9008f
sangm <sang.mercado@gmail.com>
2017-05-01 01:12:13 -0700

    Initial Implmentation with unit tests

--- addon/mixins/scoped-query-cache.js
+++ addon/mixins/scoped-query-cache.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import sortedObject from 'ember-scoped-query-cache/utils/sorted-object';
+const { assert } = Ember;
 
 export default Ember.Mixin.create({
   store: Ember.inject.service(),
@@ -8,16 +10,68 @@ export default Ember.Mixin.create({
     this.cache = {};
   },
 
-  queryRecord() {
-    return this.get('store').queryRecord(...arguments);
+  queryRecord(modelName, query, options = {}) {
+    const queryRecordMethod = () => this.get('store').queryRecord(modelName, query);
+    return this.queryCache(modelName, query, options, queryRecordMethod);
   },
 
-  query() {
-    return this.get('store').query(...arguments);
+  query(modelName, query, options = {}) {
+    const queryMethod = () => this.get('store').query(modelName, query);
+    return this.queryCache(modelName, query, options, queryMethod);
   },
 
   findRecord() {
-    // Discuss what we need to do here..
+    // TODO: Discuss what we need to do here..
     return this.get('store').findRecord(...arguments);
+  },
+
+  getStringifiedQuery(query, formatCacheKey) {
+    const queryCache = typeof(formatCacheKey) === 'function' ?
+          formatCacheKey(query) :
+          query;
+
+    assert('queryCache needs to be an object', Ember.typeOf(queryCache) === 'object');
+    return JSON.stringify(sortedObject(queryCache));
+  },
+
+  queryCache(modelName, query, options = {}, queryMethod) {
+    const { formatCacheKey, shouldCachePredicate } = options;
+    /**
+     * Let's assume modelName = 'foobar'
+     * The initial state for `this.cache` is {}
+     * We need to insert into cache resulting in
+     * cache = { 'foobar': {} }
+     */
+    if (!this.cache[modelName]) {
+      this.cache[modelName] = {};
+    }
+
+    /**
+     * Convert the object to a string with sorted keys
+     * Check if we have a cache hit, and if not, insert into cache
+     */
+    const cacheReadStringifiedQuery = this.getStringifiedQuery(query, formatCacheKey);
+    const queriesForModel = this.cache[modelName];
+    const cacheHit = queriesForModel[cacheReadStringifiedQuery];
+
+    if (cacheHit) {
+      return cacheHit;
+    }
+
+    const queryPromise = queryMethod();
+
+    // TODO: need to consider what to do if this promise fails
+    return queryPromise.then(results => {
+      const shouldCache = typeof(shouldCachePredicate) === 'function' ?
+        shouldCachePredicate(query, results) :
+        true;
+      assert('shouldCache needs to be a boolean value', typeof(shouldCache) === 'boolean');
+
+      if (shouldCache) {
+        queriesForModel[cacheReadStringifiedQuery] = queryPromise;
+      }
+
+      return results;
+    });
   }
 });
--- tests/unit/mixins/scoped-query-cache-test.js
+++ tests/unit/mixins/scoped-query-cache-test.js
@@ -24,6 +24,11 @@ module('Unit | Mixin | scoped query cache', {
       queryRecord: this.queryRecordStub,
       query: this.queryStub,
     };
+
+    const scopedQueryCacheObject = Ember.Object.extend(ScopedQueryCacheMixin);
+    this.subject = scopedQueryCacheObject.create({
+      store: this.storeStub
+    });
   },
 
   afterEach() {
@@ -31,27 +36,106 @@ module('Unit | Mixin | scoped query cache', {
   }
 });
 
-// Replace this with your real tests.
 test('queryRecord calls store queryRecord', function(assert) {
-  const ScopedQueryCacheObject = Ember.Object.extend(ScopedQueryCacheMixin);
-  const subject = ScopedQueryCacheObject.create({
-    store: this.storeStub
-  });
-
   const queryParams = { foo: 'abc' };
-  subject.queryRecord('foobar', queryParams);
+  this.subject.queryRecord('foobar', queryParams);
 
   assert.equal(this.queryRecordStub.getCalls().length, 1);
 });
 
 test('query calls store query', function(assert) {
-  const ScopedQueryCacheObject = Ember.Object.extend(ScopedQueryCacheMixin);
-  const subject = ScopedQueryCacheObject.create({
-    store: this.storeStub
-  });
-
+  const subject = this.subject;
   const queryParams = { foo: 'abc' };
   subject.query('foobar', queryParams);
 
   assert.equal(this.queryStub.getCalls().length, 1);
 });
+
+test('cache miss calls queryRecord twice', function(assert) {
+  const subject = this.subject;
+  const queryParams = { foo: 'abc' };
+  const cacheMissQueryParams = { foo: 'abcd' };
+
+  subject.queryRecord('foobar', queryParams);
+  subject.queryRecord('foobar', cacheMissQueryParams);
+
+  assert.equal(this.queryRecordStub.getCalls().length, 2);
+});
+
+test('cache hit calls queryRecord once', function(assert) {
+  assert.expect(1);
+  const subject = this.subject;
+  const query = { a: '1' };
+
+  subject.queryRecord('foo', query);
+  subject.queryRecord('foo', query);
+  subject.queryRecord('foo', query);
+
+  assert.equal(
+    this.queryRecordStub.getCalls().length,
+    1,
+    'cache hit only executes queryRecord once'
+  );
+});
+
+test('query object order does not matter', function(assert) {
+  assert.expect(1);
+  const subject = this.subject;
+  const firstQuery = { firstArg: 'foooooooo', secondArg: 'barrrrrrrr' };
+  const secondQuery = { secondArg: 'barrrrrrrr', firstArg: 'foooooooo' };
+  subject.queryRecord('foo', firstQuery);
+  subject.queryRecord('foo', secondQuery);
+
+  assert.equal(
+    this.queryRecordStub.getCalls().length,
+    1,
+    'cache hit only executes queryRecord once'
+  );
+});
+
+test('formatCacheKey is called on query', function(assert) {
+  assert.expect(1);
+
+  const subject = this.subject;
+  const firstQuery = { firstArg: 'fooo', secondArg: 'barr' };
+  const secondQuery = { firstArg: 'fooo', secondArg: 'barr' };
+  const formatCacheKey = (queryParams) => (
+    Object
+      .keys(queryParams)
+      .reduce((finalObj, key) => {
+        const val = queryParams[key];
+        finalObj[key] = val.toUpperCase();
+        return finalObj;
+      }, {})
+  );
+
+  subject.queryRecord('foo', firstQuery, { formatCacheKey });
+  subject.queryRecord('foo', secondQuery);
+
+  assert.equal(
+    this.queryRecordStub.getCalls().length,
+    2,
+    'formatCacheKey should convert vales to uppercase and result in cache miss'
+  );
+});
+
+test('if shouldCachePredicate is false, do not insert into cache', function(assert) {
+  assert.expect(1);
+
+  const subject = this.subject;
+  const shouldCachePredicate = () => false;
+  const firstQuery = { firstArg: 'foo', secondArg: 'bar' };
+  const secondQuery = { firstArg: 'foo', secondArg: 'bar' };
+
+  subject.queryRecord('foo', firstQuery, { shouldCachePredicate });
+  subject.queryRecord('foo', secondQuery);
+
+  assert.equal(
+    this.queryRecordStub.getCalls().length,
+    2,
+    'shouldCachePredicate should make sure nothing is in cache'
+  );
+});
+
+// test('findRecord is supported');
+// test('failed promise is handled properly')
