2a40e40e9a2ec41712c9945bbe1a5780f1d9b0a0
sangm <sang.mercado@gmail.com>
2017-04-30 22:17:22 -0700

    Adding `sorted-object` util

---
+++ addon/utils/sorted-object.js
@@ -0,0 +1,9 @@
+export default function sortedObject(obj) {
+  return Object
+    .keys(obj)
+    .sort()
+    .reduce((reducedObj, key) => {
+      reducedObj[key] = obj[key];
+      return reducedObj;
+    }, {});
+}
---
+++ tests/unit/utils/sorted-object-test.js
@@ -0,0 +1,10 @@
+import { module, test } from 'qunit';
+import sortedObject from 'ember-scoped-query-cache/utils/sorted-object';
+
+module('Unit | Utility | sorted-object');
+
+test('returns object with keys sorted', (assert) => {
+  assert.deepEqual(sortedObject({}), {});
+  assert.deepEqual(sortedObject({ a: 1 }), { a: 1 });
+  assert.deepEqual(sortedObject({ b: 1, a: 1 }), { a: 1, b: 1});
+});
